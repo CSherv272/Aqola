5c28648ddc21cd524da3134a929526fcd90a1f3d
Anirudh Saravanakumar <sk.anirudh20@gmail.com>
2026-01-20 22:33:15 +0000

    AQOLAGP-161 updated schema_validation test

--- automation_scripts/run_db_test.py
+++ automation_scripts/run_db_test.py
@@ -24,6 +24,39 @@ def test_connection():
     except Exception as e:
         print(f"DEBUG: Trying to connect to {os.getenv('POSTGRES_DB')} as {os.getenv('POSTGRES_USER')}")
         print(f"CONNECTION FAILED: {e}")
+        
+def run_schema_validation():
+    try:
+        conn = get_db_connection()
+        cur = conn.cursor()
+
+        # Execute the SQL check file
+        schema_validation_sql_path = r'database/init/tests/schema_validation.sql'
+        with open(schema_validation_sql_path) as f:
+            cur.execute(f.read())
+        
+        found_tables = [row[0] for row in cur.fetchall()]
+        expected_tables = ['display_zones', 'statistical_areas', 'postcodes', 'crime_data']
+        
+        print("\n---  SCHEMA VALIDATION REPORT ---")
+        for table in expected_tables:
+            if table in found_tables:
+                print(f" PASSED: Table '{table}' found.")
+            else:
+                print(f" FAILED: Table '{table}' is missing from database.")
+        
+        # Verify PostGIS is active
+        if 'spatial_ref_sys' in found_tables:
+            print(" PASSED: PostGIS extension confirmed.")
+        else:
+            print(" FAILED: PostGIS extension missing.")
+        print("------------------------------------\n")
+
+        cur.close()
+        conn.close()
+    except Exception as e:
+        print(f" SCHEMA AUDIT ERROR: {e}")
 
 if __name__ == "__main__":
     test_connection()
+    run_schema_validation()
--- database/init/tests/schema_validation.sql
+++ database/init/tests/schema_validation.sql
@@ -1,1 +1,11 @@
-
+-- QA audit to verify all necessary tables are present according to  schema
+SELECT table_name 
+FROM information_schema.tables 
+WHERE table_schema = 'public' 
+AND table_name IN (
+    'display_zones', 
+    'statistical_areas', 
+    'postcodes', 
+    'crime_data', 
+    'spatial_ref_sys'
+);
