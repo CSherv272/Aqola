7088ee6498c5f70451e4384e2416a3b0d641a28a
Anirudh Saravanakumar <sk.anirudh20@gmail.com>
2026-01-20 20:20:44 +0000

    AQOLAGP-153 refactored run_db_test to have function db_connection

--- automation_scripts/run_db_test.py
+++ automation_scripts/run_db_test.py
@@ -5,16 +5,20 @@ from dotenv import load_dotenv
 # Loads credentials from .env file
 load_dotenv()
 
+def get_db_connection():
+    """Helper method to centralize connection logic."""
+    return psycopg2.connect(
+        host="localhost",
+        port="5431",
+        database=os.getenv("POSTGRES_DB"),
+        user=os.getenv("POSTGRES_USER"),
+        password=os.getenv("POSTGRES_PASSWORD")
+    )
+
 def test_connection():
     try:
         print(f"DEBUG: Connecting with user '{os.getenv('POSTGRES_USER')}' and password '{os.getenv('POSTGRES_PASSWORD')}'")
-        conn = psycopg2.connect(
-            host= "localhost",
-            port="5431",
-            database=os.getenv("POSTGRES_DB"),
-            user=os.getenv("POSTGRES_USER"),
-            password=os.getenv("POSTGRES_PASSWORD"),
-        )
+        conn = get_db_connection()
         print("CONNECTION SUCCESS: Python Runner connected to Docker Database!")
         conn.close()
     except Exception as e:
