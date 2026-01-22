8c48a306c5dae088fed7cd4347b3b9f092b8a161
Andrew Meyer <am2660@kent.ac.uk>
2026-01-22 15:01:10 +0000

    feat: simple data call on frontend

--- frontend/aqola-frontend/app/page.tsx
+++ frontend/aqola-frontend/app/page.tsx
@@ -7,11 +7,20 @@
 import dynamic from "next/dynamic";
 import Banner from "./components/aqola-banner";
 import D3Test from "./components/simple-d3";
+import { useState } from "react";
 
 const LazyMap = dynamic(() => import("./components/Map"), {
   ssr: false,
   loading: () => <p>Loading...</p>,
 });
+
+const [data, setData] = useState([])
+const getData = async() => {
+    const response = await fetch("http://localhost:8000/")
+    setData(await response.json())
+    console.log(data)
+}
+useState(getData())
 
 export default function Home() {
   return (
