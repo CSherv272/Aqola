361c4601a27a134346eb293debb3375f7222edaf
andrew meyer <andrew.meyer003@gmail.com>
2026-01-23 16:02:16 +0000

    chore: updated gitignore to allow frontend lib folder

---
+++ frontend/aqola-frontend/app/lib/api.ts
@@ -0,0 +1,15 @@
+import axios from "axios";
+
+export const api = axios.create({
+    baseURL: "http://localhost:8000"
+});
+
+export const hello = async () => {
+    const response = await api.get("http://localhost:8000/")
+    return response.data;
+}
+
+// export const hola = async () => {
+//     const response = await api.get("http://localhost:8000/")
+//     return response;
+// }
