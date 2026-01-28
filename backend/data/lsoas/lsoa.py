02034a6777487c8f5acabaa80efc7efb3ea7f18e
Callum Shervill <shervc272@gmail.com>
2026-01-28 23:16:34 +0000

    feat: file takes lsoa shape file and a csv to ingest into db

---
+++ backend/data/lsoas/lsoa.py
@@ -0,0 +1,200 @@
+import sys
+from pathlib import Path
+import geopandas as gpd
+import pandas as pd
+from sqlalchemy import create_engine, text
+from sqlalchemy.orm import sessionmaker
+from geoalchemy2 import Geometry, WKTElement
+
+DB_CONFIG = {
+    'host': 'localhost',
+    'port': 5432,
+    'database': 'aqola',
+    'user': 'aqola_user',
+    'password': 'mysecretpassword'
+}
+
+TARGET_CRS = "EPSG:4326"   # WGS84 (lat/long)
+KENT_LAD_CODES = [
+    'E07000105', 'E07000106', 'E07000107', 'E07000108',
+    'E07000112', 'E07000109', 'E07000110', 'E06000035',
+    'E07000111', 'E07000113', 'E07000114', 'E07000115',
+    'E07000116',
+]
+
+def create_db_connection():
+    connection_string = (
+        f"postgresql://{DB_CONFIG['user']}:{DB_CONFIG['password']}@"
+        f"{DB_CONFIG['host']}:{DB_CONFIG['port']}/{DB_CONFIG['database']}"
+    )
+    return create_engine(connection_string)
+
+def read_and_transform_lsoa_data(file_path: Path) -> gpd.GeoDataFrame:
+    """
+    Reads LSOA shapefile and transforms it to WGS84 (the standard geographic coordinate system).
+
+    Args:
+        file_path (Path): Path to the LSOA shapefile.
+
+    Returns:
+        GeoDataFrame with transformed geometries.
+    """
+    print("Reading and transforming LSOA data...")
+
+    gdf = gpd.read_file(file_path)
+
+    print("Found {len(gdf)} LSOA areas")
+    print("Original CRS:", gdf.crs)
+    print(f"Columns: {gdf.columns.tolist()}")
+
+    if gdf.crs != TARGET_CRS:
+        gdf = gdf.to_crs(TARGET_CRS)
+        print("Transformed CRS to:", TARGET_CRS)
+
+    return gdf
+
+
+def prepare_data_for_db(gdf):
+    """Prepare GeoDataFrame for database insertion."""
+    
+    print ("Preparing data for database insert.")
+
+    lsoas = []
+
+    pop_df = pd.read_csv(r"C:\Users\Callum\Downloads\LSOA_population.csv")
+
+    for idx, row in gdf.iterrows():
+        lsoa_id = row.get('LSOA21CD', None)
+        lad_code = pop_df.loc[pop_df["LSOA 2021 Code"] == lsoa_id, "LAD 2023 Code"].values[0] if lsoa_id in pop_df["LSOA 2021 Code"].values else None
+        if lad_code not in KENT_LAD_CODES:
+            continue
+
+        centroid = row.geometry.centroid
+
+        area_sq_km = row.get('Shape_Area', row.geometry.area) / 1e6
+
+        population = pop_df.loc[pop_df["LSOA 2021 Code"] == lsoa_id, "Total"].values[0] if lsoa_id in pop_df["LSOA 2021 Code"].values else None
+
+        lsoa = {
+            'lsoa_id': lsoa_id,
+            'area_name': row.get('LSOA21NM', None),
+            'population': int(population.replace("," , "")) if population is not None else None,
+            'area_sq_km': area_sq_km,
+            'centroid': WKTElement(centroid.wkt, srid=4326),
+            'geometry': WKTElement(row.geometry.wkt, srid=4326)
+        }
+
+        lsoas.append(lsoa)
+
+    return lsoas
+
+def make_csv_from_json(lsoas, output_path: Path):
+    """Utility function to create a CSV from LSOA data for inspection."""
+    df = pd.DataFrame(lsoas)
+    df.to_csv(output_path, index=False)
+    print(f"CSV written to {output_path}")
+
+
+def insert_lsoas_to_db(engine, lsoas):
+    """Ingesting LSOAs into the database."""
+
+    print("Inserting LSOAs into database...")
+    Session = sessionmaker(bind=engine)
+    session = Session()
+
+    try:
+        insert_sql = text("""
+            INSERT INTO lsoas 
+            (lsoa_id, area_name, population, area_sq_km, boundary, centroid)
+            VALUES 
+            (:lsoa_id, :area_name, :population, :area_sq_km,
+              ST_GeomFromText(:geometry, 4326), ST_GeomFromText(:centroid, 4326))
+            ON CONFLICT (lsoa_id)
+            DO UPDATE SET
+                area_name = EXCLUDED.area_name,
+                population = EXCLUDED.population,
+                area_sq_km = EXCLUDED.area_sq_km,
+                boundary = EXCLUDED.boundary,
+                centroid = EXCLUDED.centroid
+        """)
+
+        for lsoa in lsoas:
+            lsoa['geometry'] = str(lsoa['geometry'].data)
+            lsoa['centroid'] = str(lsoa['centroid'].data)
+
+        session.execute(insert_sql, lsoas)
+        print(f"Inserted/Updated {len(lsoas)} LSOAs successfully.")
+        session.commit()
+
+    except Exception as e:
+        print(f"Error preparing insert statement: {e}")
+        session.close()
+        return
+    
+    finally:
+        session.close()
+
+    print("LSOAs inserted successfully.")
+
+def validate_import(engine):
+    """Validate the data imported"""
+    print("Validating imported data...")
+
+    Session = sessionmaker(bind=engine)
+    session = Session()
+
+    try:
+        result = session.execute(text("SELECT COUNT(*) FROM lsoas;"))
+        count = result.scalar()
+        print(f"Total LSOAs in database: {count}")
+
+    finally:
+        session.close()
+
+def main():
+    """Execute database connection."""
+    
+    shapefile_path = None
+
+    if len(sys.argv) < 2:
+        shapefile_path = Path(r"C:\Users\Callum\Downloads\LSOAs_shapefile")
+        # print("Usage: python lsoa.py <path_to_lsoa_shapefile>")
+        # sys.exit(1)
+
+    # shapefile_path = Path(sys.argv[1])
+    if not shapefile_path.exists():
+        print(f"Shapefile not found at: {shapefile_path}")
+        sys.exit(1)
+
+    try:
+        
+        gdf = read_and_transform_lsoa_data(
+            shapefile_path
+        )
+
+        engine = create_db_connection()
+        Session = sessionmaker(bind=engine)
+        session = Session()
+        print("Database connection established successfully.")
+
+        lsoas = prepare_data_for_db(gdf)
+
+        # print("Writing LSOA data to CSV for inspection...")
+
+        # make_csv_from_json(lsoas, Path(r"C:\Users\Callum\Downloads\lsoas_kent.csv"))
+
+
+        print(f"Prepared {len(lsoas)} LSOAs for database insertion.")
+
+        insert_lsoas_to_db(engine, lsoas)
+
+        # validate_import(engine)
+
+    
+    except Exception as e:
+        print(f"Error -> : {e}")
+        sys.exit(1)
+
+
+if __name__ == "__main__":
+    main()
--- database/init/initialise.sql
+++ database/init/initialise.sql
@@ -3,22 +3,13 @@ CREATE EXTENSION IF NOT EXISTS postgis;
 DROP TABLE IF EXISTS postcodes;
 DROP TABLE IF EXISTS crime_data;
 DROP TABLE IF EXISTS statistical_areas;
+DROP TABLE IF EXISTS lsoas;
 DROP TABLE IF EXISTS display_zones;
 
 
-CREATE TABLE IF NOT EXISTS display_zones (
-    id SERIAL PRIMARY KEY,
-    name VARCHAR(100) NOT NULL,
-    zone_code VARCHAR(20) NOT NULL,
-    population INT,
-    area_sq_km DECIMAL(10,2),
-    boundary GEOMETRY(MULTIPOLYGON, 4326) NOT NULL,
-    centroid GEOMETRY(POINT, 4326) NOT NULL
-);
 
-CREATE TABLE IF NOT EXISTS statistical_areas (
+CREATE TABLE IF NOT EXISTS lsoas (
     lsoa_id VARCHAR(20) PRIMARY KEY,
-    display_zone_id SERIAL NOT NULL REFERENCES display_zones(id) ON DELETE CASCADE,
     area_name VARCHAR(100) NOT NULL,
     population INT,
     area_sq_km DECIMAL(10,4),
@@ -28,18 +19,19 @@ CREATE TABLE IF NOT EXISTS statistical_areas (
 
 CREATE TABLE IF NOT EXISTS postcodes (
     postcode VARCHAR(10) PRIMARY KEY,
-    stat_area_id VARCHAR(20) NOT NULL REFERENCES statistical_areas(lsoa_id) ON DELETE CASCADE,
+    lsoa_id VARCHAR(20) NOT NULL REFERENCES lsoas(lsoa_id) ON DELETE CASCADE,
     postcode_area VARCHAR(4) NOT NULL,
     postcode_district VARCHAR(4) NOT NULL,
     postcode_sector VARCHAR(5) NOT NULL,
     latitude DECIMAL(9,6) NOT NULL,
     longitude DECIMAL(9,6) NOT NULL,
-    location GEOMETRY(POINT, 4326) NOT NULL
+    boundary GEOMETRY(MULTIPOLYGON, 4326) NOT NULL,
+    centroid GEOMETRY(POINT, 4326) NOT NULL
 );
 
 CREATE TABLE IF NOT EXISTS crime_data (
-    id SERIAL PRIMARY KEY,
-    lsoa_id VARCHAR(20) NOT NULL REFERENCES statistical_areas(lsoa_id) ON DELETE CASCADE,
+    crime_id SERIAL PRIMARY KEY,
+    lsoa_id VARCHAR(20) NOT NULL REFERENCES lsoas(lsoa_id) ON DELETE CASCADE,
     date DATE NOT NULL,
     latitude DECIMAL(9,6) NOT NULL,
     longitude DECIMAL(9,6) NOT NULL,
@@ -47,25 +39,18 @@ CREATE TABLE IF NOT EXISTS crime_data (
 );
 
 
-
-INSERT INTO display_zones (name, zone_code, population, area_sq_km, boundary, centroid)
+INSERT INTO lsoas (lsoa_id, area_name, population, area_sq_km, boundary, centroid)
 VALUES
-    ('Canterbury Display Zone', 'DZ001', 5000, 5.50,
-    ST_GeomFromText('MULTIPOLYGON(((-0.580 51.275, -0.565 51.275, -0.565 51.290, -0.580 51.290, -0.580 51.275)))', 4326),
-    ST_GeomFromText('POINT(-0.5725 51.2825)', 4326)
-    );
-
-INSERT INTO statistical_areas (lsoa_id, display_zone_id, area_name, population, area_sq_km, boundary, centroid)
-VALUES
-    ('E01024101', 1, 'Canterbury 013C', 1500, 0.85,
+    ('E01024101', 'Canterbury 013C', 1500, 0.85,
     ST_GeomFromText('MULTIPOLYGON(((-0.573 51.280, -0.570 51.280, -0.570 51.282, -0.573 51.282, -0.573 51.280)))', 4326),
     ST_GeomFromText('POINT(-0.5715 51.281)', 4326)
     );
 
-INSERT INTO postcodes (stat_area_id, postcode, postcode_area, postcode_district, postcode_sector, latitude, longitude, location)
+INSERT INTO postcodes (lsoa_id, postcode, postcode_area, postcode_district, postcode_sector, latitude, longitude, centroid, boundary)
 VALUES
     ('E01024101', 'CT2 7QS', 'CT', 'CT2', 'CT2 7', 51.294936, 1.0888,
-    ST_GeomFromText('POINT(51.294936 1.0888)', 4326)
+    ST_GeomFromText('POINT(51.294936 1.0888)', 4326),
+    ST_GeomFromText('MULTIPOLYGON(((-0.573 51.280, -0.570 51.280, -0.570 51.282, -0.573 51.282, -0.573 51.280)))', 4326)
     );
 
 INSERT INTO crime_data (lsoa_id, date, latitude, longitude, crime_type)
