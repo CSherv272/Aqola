fafbaa0e2ae715c42700a0305cf174c9a369ef41
Anirudh Saravanakumar <sk.anirudh20@gmail.com>
2026-01-28 18:01:32 +0000

    AQOLAGP-153 updated db_test so it checks for port 5432 and paths corrected to sql files

--- test_scripts/run_db_test.py
+++ test_scripts/run_db_test.py
@@ -9,7 +9,7 @@ def get_db_connection():
     """Helper method to centralize connection logic."""
     return psycopg2.connect(
         host="localhost",
-        port="5431",
+        port="5432",
         database=os.getenv("POSTGRES_DB"),
         user=os.getenv("POSTGRES_USER"),
         password=os.getenv("POSTGRES_PASSWORD")
@@ -31,7 +31,7 @@ def schema_integrity_validation():
         cur = conn.cursor()
 
         # Execute the SQL check file
-        schema_validation_sql_path = r'aqola/database/init/tests/schema_integrity_validation.sql'
+        schema_validation_sql_path = r'aqola/database/init/schema_integrity_validation.sql'
         with open(schema_validation_sql_path) as f:
             sql_content = f.read()
             queries = [q.strip() for q in sql_content.split(';') if q.strip()]
@@ -153,7 +153,7 @@ def data_quality_validation():
         cur = conn.cursor()
 
         # Execute the SQL check file
-        schema_validation_sql_path = r'aqola/database/init/tests/data_quality_validation.sql'
+        schema_validation_sql_path = r'aqola/database/init/data_quality_validation.sql'
         with open(schema_validation_sql_path) as f:
             sql_content = f.read()
             queries = [q.strip() for q in sql_content.split(';') if q.strip()]
