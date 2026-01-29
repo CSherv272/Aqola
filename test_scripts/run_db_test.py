e27ce19fbf61df5350cafe8927bb86e2fd57eafe
Anirudh Saravanakumar <sk.anirudh20@gmail.com>
2026-01-29 13:40:45 +0000

    AQOLAGP-153 fixed relative paths and added requirements.txt

--- test_scripts/run_db_test.py
+++ test_scripts/run_db_test.py
@@ -37,7 +37,7 @@ def schema_integrity_validation():
         cur = conn.cursor()
 
         # Execute the SQL check file
-        schema_validation_sql_path = r'aqola/database/init/schema_integrity_validation.sql'
+        schema_validation_sql_path = schema_validation_path
         with open(schema_validation_sql_path) as f:
             sql_content = f.read()
             queries = [q.strip() for q in sql_content.split(';') if q.strip()]
@@ -159,7 +159,7 @@ def data_quality_validation():
         cur = conn.cursor()
 
         # Execute the SQL check file
-        schema_validation_sql_path = r'aqola/database/init/data_quality_validation.sql'
+        schema_validation_sql_path = data_quality_validation_path
         with open(schema_validation_sql_path) as f:
             sql_content = f.read()
             queries = [q.strip() for q in sql_content.split(';') if q.strip()]
