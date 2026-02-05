8464478cb694c8ecb32078ef59beeb03f623637e
Andrew Meyer <am2660@kent.ac.uk>
2026-02-05 12:30:01 +0000

    feat: working on line graph data import

--- frontend/aqola-frontend/app/page.tsx
+++ frontend/aqola-frontend/app/page.tsx
@@ -25,7 +25,6 @@ const LeafletMap = dynamic(() => import("./components/maps"), {
   loading: () => <p>Loading...</p>,
 });
 
-
 const LineGraph = dynamic(() => import("./components/linegraph"), {
   ssr: false,
   loading: () => <p>Loading...</p>,
@@ -33,56 +32,55 @@ const LineGraph = dynamic(() => import("./components/linegraph"), {
 
 export default function Home() {
   //app state variables
-  let [selectedPostcodes, setSelectedPostcodes] = useState([])
-  let [selectedDataSet, setSelectedDataSet] = useState("crime_data")
+  let [selectedPostcodes, setSelectedPostcodes] = useState([]);
+  let [selectedDataSet, setSelectedDataSet] = useState("crime_data");
 
   const handleLineHover = (newValue: string) => {
-    setSelectedDataSet(newValue)
-    console.log("selected dataset", selectedDataSet)
-  }
+    setSelectedDataSet(newValue);
+    console.log("selected dataset", selectedDataSet);
+  };
 
-  const [data, setData] = useState([])
-  const [showGraph, setShowGraph] = useState(false)
-  const [postcode, setPostcode] = useState([])
+  const [data, setData] = useState([]);
+  const [showGraph, setShowGraph] = useState(false);
+  const [postcode, setPostcode] = useState([]);
 
   const getPostcode = async () => {
-    const response = await getPostcodeData("CT27QS")
-    setPostcode(response)
-  }
-
+    const response = await getPostcodeData("CT27QS");
+    setPostcode(response);
+  };
 
   // retrieves data through an api.ts function
   const getData = async () => {
-    const response = await hello()
-    setData(response.message)
-    setShowGraph(!showGraph)
-  }
+    const response = await hello();
+    setData(response.message);
+    setShowGraph(!showGraph);
+  };
 
   // when data changes, update is printed to console
   useEffect(() => {
-    console.log("your data", data)
-  }, [data])
+    console.log("your data", data);
+  }, [data]);
 
   useEffect(() => {
     const postcode_data = getPostcode();
-  }, [])
+  }, []);
 
   useEffect(() => {
-    console.log("your postcode data", postcode)
-  }, [postcode])
+    console.log("your postcode data", postcode);
+  }, [postcode]);
 
   let crime_data = {
-    "chart_type": "line",
-    "type": "crime_data",
-    "area": "postcodes",
-    "chart": {
-      "lines": [
+    chart_type: "line",
+    type: "crime_data",
+    area: "postcodes",
+    chart: {
+      lines: [
         {
           line_name: "Drugs",
           coords: [
             [0, 10],
             [1, 20],
-            [2, 30]
+            [2, 30],
           ],
         },
         {
@@ -90,39 +88,46 @@ export default function Home() {
           coords: [
             [0, 5],
             [1, 15],
-            [2, 25]
+            [2, 25],
           ],
-        }
+        },
       ],
-      "title": "Crime by Postcode",
-      "xlabel": "Time (months)",
-      "ylabel": "Number of Crimes",
-    }
-  }
+      title: "Crime by Postcode",
+      xlabel: "Time (months)",
+      ylabel: "Number of Crimes",
+    },
+  };
 
   let colours = {
-    "Burglary": "blue",
-    "Robbery": "red",
+    Burglary: "blue",
+    Robbery: "red",
     "Vehicle Crime": "green",
     "Violent Crime": "orange",
     "Other Crime": "purple",
     "Anti-social Behaviour": "brown",
     "Criminal Damage": "pink",
-    "Drugs": "cyan",
+    Drugs: "cyan",
     "Public Order": "magenta",
-    "Shoplifting": "yellow",
-    "Theft": "grey",
+    Shoplifting: "yellow",
+    Theft: "grey",
     "Bicycle Theft": "black",
     "Possession of Weapons": "lime",
     "Other Theft": "teal",
     "All Crime": "navy",
     "Criminal Damage and Arson": "maroon",
-  }
+  };
 
   return (
     <div>
       <Banner trigger={getData} /> {/*trigger is button press*/}
-      {showGraph && <LineGraph data={crime_data} colours={colours} get_line_name={handleLineHover} />} {/*show and hide map*/}
+      {showGraph && (
+        <LineGraph
+          data={crime_data}
+          colours={colours}
+          get_line_name={handleLineHover}
+        />
+      )}{" "}
+      {/*show and hide map*/}
       <LeafletMap />
     </div>
   );
