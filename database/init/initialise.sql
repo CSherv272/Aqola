f41bf87de41477ffe550a83524e93f95e186d2b9
Anirudh Saravanakumar <sk.anirudh20@gmail.com>
2026-02-07 11:31:30 +0000

    AQOLAGP-101 updated create table

--- database/init/initialise.sql
+++ database/init/initialise.sql
@@ -47,9 +47,9 @@ CREATE TABLE IF NOT EXISTS flood_data (
 );
 
 CREATE TABLE IF NOT EXISTS school_data (
-    urn INT PRIMARY KEY,
+    urn VARCHAR(20) NOT NULL,
     lsoa_id VARCHAR(20) NOT NULL REFERENCES lsoas(lsoa_id) ON DELETE CASCADE,
-    school_name VARCHAR(100) NOT NULL,
+    school_name VARCHAR(255) NOT NULL,
     postcode VARCHAR(10) NOT NULL REFERENCES postcodes(postcode) ON DELETE CASCADE,
     is_primary BOOLEAN NOT NULL,
     is_secondary BOOLEAN NOT NULL,
@@ -57,9 +57,11 @@ CREATE TABLE IF NOT EXISTS school_data (
     gender VARCHAR(6) NOT NULL,
     year_range VARCHAR(10) NOT NULL,
     ofsted_ranking INT,
-    centroid GEOMETRY(POINT, 4326) NOT NULL,
-    latitude DECIMAL(9,6) NOT NULL,
-    longitude DECIMAL(9,6) NOT NULL
+    centroid GEOMETRY(POINT, 4326),
+    latitude DECIMAL(9,6),
+    longitude DECIMAL(9,6),
+
+    PRIMARY KEY (urn, year_range)
 );
 
 
