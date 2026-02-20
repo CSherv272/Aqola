fe53bc35bdad143bde583fd524152d3b27fa84e8
Anirudh Saravanakumar <sk.anirudh20@gmail.com>
2026-02-20 14:03:45 +0000

    AQOLAGP-101 added school data class to db_models

--- backend/api/models/db_models.py
+++ backend/api/models/db_models.py
@@ -1,4 +1,4 @@
-from sqlalchemy import Column, String, Float, Integer, ForeignKey, Index
+from sqlalchemy import Column, String, Float, Integer, ForeignKey, Index, Boolean, DECIMAL
 from sqlalchemy.dialects.postgresql import UUID
 from sqlalchemy.orm import relationship
 from geoalchemy2 import Geometry
@@ -23,3 +23,22 @@ class Postcode(Base):
     
     # # Relationships
     # zone = relationship("DisplayZone", back_populates="postcodes")
+
+class SchoolData(Base):
+    __tablename__ = "school_data"
+
+    urn = Column(String(20), primary_key=True, nullable=False)
+    year_range = Column(String(10), primary_key=True, nullable=False)
+    school_name = Column(String(255), nullable=False)
+    lsoa_id = Column(String(20), index=True)
+    postcode = Column(String(10), index=True)
+    is_primary = Column(Boolean, nullable=False)
+    is_secondary = Column(Boolean, nullable=False)
+    is_post16 = Column(Boolean, nullable=False)
+    gender = Column(String(6), nullable=False)
+    ofsted_ranking = Column(Integer)
+    latitude = Column(DECIMAL(9, 6))
+    longitude = Column(DECIMAL(9, 6))
+    centroid = Column(Geometry('POINT', srid=4326))
+    
+    
