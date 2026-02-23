dca76da72caba6d3e7fbf29cf433c82f44ad73a6
Callum Shervill <shervc272@gmail.com>
2026-02-23 15:36:01 +0000

    chore: have data passed in and colour passed in

--- frontend/aqola-frontend/app/components/bargraph.jsx
+++ frontend/aqola-frontend/app/components/bargraph.jsx
@@ -4,76 +4,7 @@ import * as d3 from "d3";
 import { useRef, useEffect } from "react";
 
 export default function BarGraph({
-  data = {
-    groups: [
-      {
-        name: "CT2 7QS",
-        bars: [
-          {
-            bar_name: "high_risk",
-            value: 30,
-          },
-          {
-            bar_name: "medium_risk",
-            value: 12,
-          },
-          {
-            bar_name: "low_risk",
-            value: 40,
-          },
-          {
-            bar_name: "very_low_risk",
-            value: 50,
-          },
-        ],
-      },
-      {
-        name: "CT2 7QB",
-        bars: [
-          {
-            bar_name: "high_risk",
-            value: 45,
-          },
-          {
-            bar_name: "medium_risk",
-            value: 64,
-          },
-          {
-            bar_name: "low_risk",
-            value: 20,
-          },
-          {
-            bar_name: "very_low_risk",
-            value: 3,
-          },
-        ],
-      },
-      {
-        name: "CT2 7QA",
-        bars: [
-          {
-            bar_name: "high_risk",
-            value: 45,
-          },
-          {
-            bar_name: "medium_risk",
-            value: 64,
-          },
-          {
-            bar_name: "low_risk",
-            value: 20,
-          },
-          {
-            bar_name: "very_low_risk",
-            value: 3,
-          },
-        ],
-      },
-    ],
-    title: "Flood data bargraph!",
-    xlabel: "Postcodes",
-    ylabel: "Number of Houses at risk",
-  },
+  data,
   marginTop = 20,
   marginRight = 20,
   marginBottom = 30,
@@ -87,18 +18,23 @@ export default function BarGraph({
 
   const innerWidth = width - marginLeft - marginRight;
   const innerHeight = height - marginTop - marginBottom;
-  const postcode_subgroups = d3.map(data.groups[0].bars, (bar) => bar.bar_name);
-  const postcode_groups = d3.map(data.groups, (group) => group.name);
 
-  console.log(postcode_subgroups + " SUBGROUPS");
-  console.log(postcode_groups + " GROUPS");
+  // subgroups are the labels of the bars IN the bar graph groups (i.e. high risk, low risk)
+  const subgroups = d3.map(data.groups[0].bars, (bar) => bar.bar_name);
+  // groups are the labels of bar groups (in this case postcodes)
+  const groups = d3.map(data.groups, (group) => group.name);
 
+  console.log(subgroups + " SUBGROUPS");
+  console.log(groups + " GROUPS");
+
+  //Create the data scale for the x axis
   const xScale = d3
     .scaleBand()
-    .domain(postcode_groups)
+    .domain(groups)
     .range([0, innerWidth])
     .padding(0.2);
 
+  //Create the data scale for the y axis
   const yScale = d3
     .scaleLinear()
     .domain([
@@ -107,15 +43,17 @@ export default function BarGraph({
     ])
     .range([innerHeight, 0]);
 
+  //Create the subgroup data scale
   const xSubgroupScale = d3
     .scaleBand()
-    .domain(postcode_subgroups)
+    .domain(subgroups)
     .range([0, xScale.bandwidth()])
     .padding(0.1);
 
+  //Color scale helps picks colours base on another group (this case subgroups)
   const colourScale = d3
     .scaleOrdinal()
-    .domain(postcode_subgroups)
+    .domain(subgroups)
     .range(["#dc0000", "#e7b416", "#fff500", "#2dc937"]);
 
   useEffect(
@@ -134,6 +72,7 @@ export default function BarGraph({
   return (
     <svg width={width} height={height}>
       <g transform={`translate(${marginLeft}, ${marginTop})`}>
+        {/* Create the lines going across the background */}
         {yScale.ticks().map((tickValue) => (
           <line
             x1={0}
@@ -145,8 +84,10 @@ export default function BarGraph({
           />
         ))}
 
+        {/* For every group, scale across the x based on the xscale */}
         {data.groups.map((group, index) => (
           <g transform={`translate(${xScale(group.name)}, 0)`} key={group.name}>
+            {/* For every bar in subgroup, scale across starting from  */}
             {group.bars.map((bar, key) => (
               <rect
                 x={xSubgroupScale(bar.bar_name)}
@@ -155,7 +96,7 @@ export default function BarGraph({
                 height={innerHeight - yScale(bar.value)}
                 key={key}
                 id=""
-                fill={colourScale(bar.bar_name)}
+                fill={bar.color}
                 className="hover:saturate-300 focus:outline-2 transition-transform duration-300"
               />
             ))}
