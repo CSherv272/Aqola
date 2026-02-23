bec9de9695c11bcf69bb3b6d6fb25ed29f2ab0fb
Anirudh Saravanakumar <sk.anirudh20@gmail.com>
2026-02-23 16:50:15 +0000

    AQOLAGP-103 smoke test for school data json

--- backend/api/tests/routertests/test_postcodes.py
+++ backend/api/tests/routertests/test_postcodes.py
@@ -14,3 +14,14 @@ def test_get_postcode_valid():
     assert data[0]["postcode"] == "CT2 7QS"
     assert "latitude" in data[0]
     assert "longitude" in data[0]
+    
+def test_get_school_data_valid():
+    response = client.get(f"/postcodes/DA28DH/schools")
+    assert response.status_code == 200
+    data = response.json()
+    assert data[0]["school_name"] == "Darenth Community Primary School"
+    assert "ofsted_ranking" in data[0]
+    assert "is_primary" in data[0]
+    assert "is_secondary" in data[0]
+    assert "is_post16" in data[0]
+    assert "gender" in data[0]
