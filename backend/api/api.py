6d451297f9df66b69059e33d0a145a827e8d9f9d
Andrew Meyer <am2660@kent.ac.uk>
2026-02-24 22:45:18 +0000

    feat: added database table names function

--- backend/api/api.py
+++ backend/api/api.py
@@ -9,6 +9,7 @@ from api.routers.lsoa import school as school_lsoa
 from api.routers.full_datasets import crime as crime_full
 from api.routers.full_datasets import flood as flood_full
 from api.routers.full_datasets import school as school_full
+from api.routers.full_datasets import database
 
 origins ={
     "http://localhost:3000",
@@ -40,4 +41,4 @@ app.include_router(school_lsoa.router, prefix="/lsoas")
 app.include_router(school_pcd.router, prefix="/postcodes")
 app.include_router(crime_full.router, prefix="/crime")
 app.include_router(flood_full.router, prefix="/flood")
-app.include_router(school_full.router, prefix="/school")
+app.include_router(database.router, prefix="/database")
