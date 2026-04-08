30191d848b8a8e53d8061dbd6bb2d37036d48a09
Callum Shervill <shervc272@gmail.com>
2026-04-08 10:36:33 +0100

    feat: api calls for lsoa polygons

--- backend/api/routers/lsoas.py
+++ backend/api/routers/lsoas.py
@@ -3,8 +3,13 @@ from sqlalchemy.orm import Session
 from api.database import get_db
 from sqlalchemy import func
 from typing import List, Optional
+import json
 
-from api.models.response_models.lsoa import LsoaResponse
+
+from geoalchemy2.functions import ST_AsGeoJSON, ST_Intersects, ST_MakeEnvelope
+
+from api.models.response_models.polygon import GeometryModel
+from api.models.response_models.lsoa import LsoaResponse, LsoaPolygonResponse
 from api.models.db_models import Lsoa
 
 router = APIRouter()
@@ -37,3 +42,38 @@ async def list_lsoas(
         )
         for result in results
     ]
+
+@router.get("/geometry", response_model=List[LsoaPolygonResponse])
+async def list_lsoas(min_lat: float, max_lat: float, min_lng: float, max_lng: float, db: Session = Depends(get_db)):
+    """List postcodes"""
+    
+    print(f"Bounds received: min_lat={min_lat}, max_lat={max_lat}, min_lng={min_lng}, max_lng={max_lng}")
+
+
+    lsoas = (
+        db.query(
+            Lsoa.lsoa_id,
+            ST_AsGeoJSON(Lsoa.boundary).label("boundary")
+        ).filter (
+            ST_Intersects(
+                Lsoa.boundary,
+                ST_MakeEnvelope(min_lng, min_lat, max_lng, max_lat, 4326)
+            )
+        )
+        .all()
+    )
+
+    if not lsoas:
+        raise HTTPException(status_code=404, detail="No records found. Double check the lsoa(s) entered")
+
+    lsoa_polygons = []
+
+    if len(lsoas) == 0:
+        return lsoa_polygons    
+
+    for lsoa_record in lsoas:
+        boundary_json = json.loads(lsoa_record.boundary)
+        lsoa_polygons.append(LsoaPolygonResponse(boundary=GeometryModel(type=boundary_json["type"], coordinates=boundary_json["coordinates"]), lsoa=lsoa_record.lsoa_id))
+
+
+    return lsoa_polygons
