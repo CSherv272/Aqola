f3e31dcdfbf8f9838dc7bc65f3a8e269976790ba
Andrew Meyer <am2660@kent.ac.uk>
2026-04-13 14:01:41 +0100

    fix: multiple states on graph load

--- frontend/aqola-frontend/app/components/Charts.tsx
+++ frontend/aqola-frontend/app/components/Charts.tsx
@@ -43,7 +43,7 @@ export default function Charts() {
         }
 
         buildCharts();
-    }, [charts, selectedAreas]); // Rebuild charts whenever the stack changes or selected areas change (for live updates)
+    }, [charts]); // Rebuild charts whenever the stack changes or selected areas change (for live updates)
 
     return <>{chartElements}</>;
 }
