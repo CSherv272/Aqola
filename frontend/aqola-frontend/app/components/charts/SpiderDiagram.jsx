ece4e33ca4b1a1ee021ad0654c5cd6a38d2f0261
andrew meyer <andrew.meyer003@gmail.com>
2026-04-24 14:30:10 +0100

    feat: spider diagram

--- frontend/aqola-frontend/app/components/charts/SpiderDiagram.jsx
+++ frontend/aqola-frontend/app/components/charts/SpiderDiagram.jsx
@@ -1,322 +1,125 @@
-/////////////////////////////////////////////////////////
-/////////////// The Radar Chart Function ////////////////
-/////////////// Written by Nadieh Bremer ////////////////
-////////////////// VisualCinnamon.com ///////////////////
-/////////// Inspired by the code of alangrafu ///////////
-/////////////////////////////////////////////////////////
-// https://d3-graph-gallery.com/spider
-
 import * as d3 from "d3";
 import { useRef, useEffect } from "react";
 
-// var data = [
-//             [//iPhone
-//             {axis:"Battery Life",value:0.22},
-//             {axis:"Brand",value:0.28},
-//             {axis:"Contract Cost",value:0.29},
-//             {axis:"Design And Quality",value:0.17},
-//             {axis:"Have Internet Connectivity",value:0.22},
-//             {axis:"Large Screen",value:0.02},
-//             {axis:"Price Of Device",value:0.21},
-//             {axis:"To Be A Smartphone",value:0.50}			
-//             ],[//Samsung
-//             {axis:"Battery Life",value:0.27},
-//             {axis:"Brand",value:0.16},
-//             {axis:"Contract Cost",value:0.35},
-//             {axis:"Design And Quality",value:0.13},
-//             {axis:"Have Internet Connectivity",value:0.20},
-//             {axis:"Large Screen",value:0.13},
-//             {axis:"Price Of Device",value:0.35},
-//             {axis:"To Be A Smartphone",value:0.38}
-//             ],[//Nokia Smartphone
-//             {axis:"Battery Life",value:0.26},
-//             {axis:"Brand",value:0.10},
-//             {axis:"Contract Cost",value:0.30},
-//             {axis:"Design And Quality",value:0.14},
-//             {axis:"Have Internet Connectivity",value:0.22},
-//             {axis:"Large Screen",value:0.04},
-//             {axis:"Price Of Device",value:0.41},
-//             {axis:"To Be A Smartphone",value:0.30}
-//             ]
-//         ];	
-
 function RadarChart({ data }) {
-  const containerRef = useRef(null);
+	// Refs for SVG elements
+	const gRef = useRef();
+	const chartRef = useRef();
 
-  	useEffect(() => {
-		if (!containerRef.current) return;
+	const width = 600;
+	const height = 600;
+	const margin = { top: 80, right: 80, bottom: 80, left: 80 };
+	const levels = 5;
+	const labelFactor = 1.25;
+	const color = d3.scaleOrdinal(d3.schemeCategory10);
 
-		const cfg = {
-		w: 600,
-		h: 600,
-		margin: { top: 20, right: 20, bottom: 20, left: 20 },
-		levels: 3,
-		maxValue: 0,
-		labelFactor: 1.25,
-		wrapWidth: 60,
-		opacityArea: 0.35,
-		dotRadius: 4,
-		opacityCircles: 0.1,
-		strokeWidth: 2,
-		roundStrokes: false,
-		color: d3.scaleOrdinal(d3.schemeCategory10),
-		};
+	// Calculate the radius of the radar chart
+	const radius = Math.min((width - margin.left - margin.right) / 2, (height - margin.top - margin.bottom) / 2);
 
-		const container = d3.select(containerRef.current);
-		container.select("svg").remove();
-		
-		//Put all of the options into a variable called cfg
-		// if('undefined' !== typeof options){
-		//   for(var i in options){
-		// 	if('undefined' !== typeof options[i]){ cfg[i] = options[i]; }
-		//   }//for i
-		// }//if
-		
-		//If the supplied maxValue is smaller than the actual one, replace by the max in the data
-		var maxValue = Math.max(cfg.maxValue, d3.max(data.chart.groups.flat(), (i) => i.value));
-			
-		var allAxis = (data.chart.groups[0].map(function(i, j){return i.axis})),	//Names of each axis
-			total = allAxis.length,					//The number of different axes
-			radius = Math.min(cfg.w/2, cfg.h/2), 	//Radius of the outermost circle
-			Format = d3.format('.0%'),			 	//Percentage formatting
-			angleSlice = Math.PI * 2 / total;		//The width in radians of each "slice"
-		
-		//Scale for the radius
-		var rScale = d3.scaleLinear()
-			.range([0, radius])
-			.domain([0, maxValue]);
-			
-		/////////////////////////////////////////////////////////
-		//////////// Create the container SVG and g /////////////
-		/////////////////////////////////////////////////////////
+	// Find the max value in data, get axes labels, and calculate angles between axes
+	const maxValue = d3.max(data.chart.groups.flat(), (i) => i.value) ?? 0;
+	const axesLabels = data.chart.groups[0].map((i) => i.axis);
+	const sectionAngle = (Math.PI * 2) / axesLabels.length;
 
-		//Remove whatever chart with the same id/class was present before
-		container.select("svg").remove();
+	// Set a scale for the attributes based on the max value and radius
+	const attributeScale = d3.scaleLinear().range([0, radius]).domain([0, maxValue]);
 
-		//Initiate the radar chart SVG
-		var svg = container.append("svg")
-			.attr("width",  "100%")
-			.attr("height", "100%")
-			.attr("class", "radarChart")
-			.attr("viewbox", `0 0 ${cfg.w} ${cfg.h}`)
-			.attr("preserveAspectRatio", "xMidYMid meet");
-		//Append a g element		
-		var g = svg.append("g")
-				.attr("transform", "translate(" + (cfg.w/2 + cfg.margin.left) + "," + (cfg.h/2 + cfg.margin.top) + ")");
-		
-		/////////////////////////////////////////////////////////
-		////////// Glow filter for some extra pizzazz ///////////
-		/////////////////////////////////////////////////////////
-		
-		//Filter for the outside glow
-		var filter = g.append('defs').append('filter').attr('id','glow'),
-			feGaussianBlur = filter.append('feGaussianBlur').attr('stdDeviation','2.5').attr('result','coloredBlur'),
-			feMerge = filter.append('feMerge'),
-			feMergeNode_1 = feMerge.append('feMergeNode').attr('in','coloredBlur'),
-			feMergeNode_2 = feMerge.append('feMergeNode').attr('in','SourceGraphic');
+	// Function for creating attribute lines
+	const attributeLine = d3.lineRadial()
+		.curve(d3.curveLinearClosed)
+		.radius((d) => attributeScale(d.value))
+		.angle((_, i) => i * sectionAngle);
 
-		/////////////////////////////////////////////////////////
-		/////////////// Draw the Circular grid //////////////////
-		/////////////////////////////////////////////////////////
-		
-		//Wrapper for the grid & axes
-		var axisGrid = g.append("g").attr("class", "axisWrapper");
-		
-		//Draw the background circles
-		axisGrid.selectAll(".levels")
-		.data(d3.range(1,(cfg.levels+1)).reverse())
-		.enter()
+	useEffect(() => {
+		if (!gRef.current || data.chart.groups.length === 0) return;
+
+		// Axis
+		const chart = d3.select(chartRef.current);
+		chart.selectAll("*").remove();
+
+		// Plot the level circles
+		chart.selectAll(".levels")
+			.data(d3.range(1, levels + 1).reverse())
+			.enter()
 			.append("circle")
-			.attr("class", "gridCircle")
-			.attr("r", function(d, i){return radius/cfg.levels*d;})
+			.attr("r", (d) => (radius / levels) * d)
 			.style("fill", "#CDCDCD")
 			.style("stroke", "#CDCDCD")
-			.style("fill-opacity", cfg.opacityCircles)
-			.style("filter" , "url(#glow)");
+			.style("fill-opacity", 0.1);
 
-		//Text indicating at what % each level is
-		axisGrid.selectAll(".axisLabel")
-		.data(d3.range(1,(cfg.levels+1)).reverse())
-		.enter().append("text")
-		.attr("class", "axisLabel")
-		.attr("x", 4)
-		.attr("y", function(d){return -d*radius/cfg.levels;})
-		.attr("dy", "0.4em")
-		.style("font-size", "10px")
-		.attr("fill", "#737373")
-		.text(function(d,i) { return Format(maxValue * d/cfg.levels); });
+		// Plot the level labels
+		chart.selectAll(".axisLabel")
+			.data(d3.range(1, levels + 1).reverse())
+			.enter()
+			.append("text")
+			.attr("x", 4)
+			.attr("y", (d) => (-d * radius) / levels)
+			.attr("dy", "0.4em")
+			.style("font-size", "10px")
+			.attr("fill", "#737373")
+			.text((d) => d3.format(".0%")(maxValue * (d / levels)));
 
-		/////////////////////////////////////////////////////////
-		//////////////////// Draw the axes //////////////////////
-		/////////////////////////////////////////////////////////
-		
-		//Create the straight lines radiating outward from the center
-		var axis = axisGrid.selectAll(".axis")
-			.data(allAxis)
+		// Create axis plot
+		const axis = chart.selectAll(".axis")
+			.data(axesLabels)
 			.enter()
 			.append("g")
-			.attr("class", "axis");
-		//Append the lines
+			.attr("width", "100%")
+			.attr("height", "100%");
+
+		// Add each axis line for the features
 		axis.append("line")
-			.attr("x1", 0)
-			.attr("y1", 0)
-			.attr("x2", function(d, i){ return rScale(maxValue*1.1) * Math.cos(angleSlice*i - Math.PI/2); })
-			.attr("y2", function(d, i){ return rScale(maxValue*1.1) * Math.sin(angleSlice*i - Math.PI/2); })
-			.attr("class", "line")
+			.attr("x1", 0).attr("y1", 0)
+			.attr("x2", (_, i) => attributeScale(maxValue * 1.1) * Math.cos(sectionAngle * i - Math.PI / 2))
+			.attr("y2", (_, i) => attributeScale(maxValue * 1.1) * Math.sin(sectionAngle * i - Math.PI / 2))
 			.style("stroke", "white")
 			.style("stroke-width", "2px");
-
-		//Append the labels at each axis
+		
+		// Add labels for each axis
 		axis.append("text")
-			.attr("class", "legend")
 			.style("font-size", "11px")
 			.attr("text-anchor", "middle")
 			.attr("dy", "0.35em")
-			.attr("x", function(d, i){ return rScale(maxValue * cfg.labelFactor) * Math.cos(angleSlice*i - Math.PI/2); })
-			.attr("y", function(d, i){ return rScale(maxValue * cfg.labelFactor) * Math.sin(angleSlice*i - Math.PI/2); })
-			.text(function(d){return d})
-			.call(wrap, cfg.wrapWidth);
+			.attr("x", (_, i) => attributeScale(maxValue * 1.35) * Math.cos(sectionAngle * i - Math.PI / 2))
+			.attr("y", (_, i) => attributeScale(maxValue * 1.35) * Math.sin(sectionAngle * i - Math.PI / 2))
+			.text((d) => d);
+	}, [data]);
 
-		/////////////////////////////////////////////////////////
-		///////////// Draw the radar chart blobs ////////////////
-		/////////////////////////////////////////////////////////
-		
-		//The radial line function
-		var radarLine = d3.lineRadial()
-			.curve(d3.curveLinearClosed)
-			.radius(function(d) { return rScale(d.value); })
-			.angle(function(d,i) {	return i*angleSlice; });
-			
-		if(cfg.roundStrokes) {
-			radarLine.curve(d3.curveCardinalClosed);
-		}
-					
-		//Create a wrapper for the blobs	
-		var blobWrapper = g.selectAll(".radarWrapper")
-			.data(data.chart.groups)
-			.enter().append("g")
-			.attr("class", "radarWrapper");
-				
-		//Append the backgrounds	
-		blobWrapper
-			.append("path")
-			.attr("class", "radarArea")
-			.attr("d", function(d,i) { return radarLine(d); })
-			.style("fill", function(d,i) { return cfg.color(i); })
-			.style("fill-opacity", cfg.opacityArea)
-			.on('mouseover', function (d,i){
-				//Dim all blobs
-				d3.selectAll(".radarArea")
-					.transition().duration(200)
-					.style("fill-opacity", 0.1); 
-				//Bring back the hovered over blob
-				d3.select(this)
-					.transition().duration(200)
-					.style("fill-opacity", 0.7);	
-			})
-			.on('mouseout', function(){
-				//Bring back all blobs
-				d3.selectAll(".radarArea")
-					.transition().duration(200)
-					.style("fill-opacity", cfg.opacityArea);
-			});
-			
-		//Create the outlines	
-		blobWrapper.append("path")
-			.attr("class", "radarStroke")
-			.attr("d", function(d,i) { return radarLine(d); })
-			.style("stroke-width", cfg.strokeWidth + "px")
-			.style("stroke", function(d,i) { return cfg.color(i); })
-			.style("fill", "none")
-			.style("filter" , "url(#glow)");		
-		
-		//Append the circles
-		blobWrapper.selectAll(".radarCircle")
-			.data(function(d,i) { return d; })
-			.enter().append("circle")
-			.attr("class", "radarCircle")
-			.attr("r", cfg.dotRadius)
-			.attr("cx", function(d,i){ return rScale(d.value) * Math.cos(angleSlice*i - Math.PI/2); })
-			.attr("cy", function(d,i){ return rScale(d.value) * Math.sin(angleSlice*i - Math.PI/2); })
-			.style("fill", function(d,i,j) { return cfg.color(j); })
-		.style("fill-opacity", 0.8);
+	return (
+	<svg
+		viewBox={`0 0 ${width} ${height}`}
+		style={{ width: "100%", height: "100%", display: "block" }}
+		preserveAspectRatio="xMidYMid meet"
+	>
+		<g transform={`translate(${width / 2}, ${height / 2})`} ref={gRef} width={"100%"} height={"100%"}>
+		<g ref={chartRef} width={"100%"} height={"100%"} />
 
-		/////////////////////////////////////////////////////////
-		//////// Append invisible circles for tooltip ///////////
-		/////////////////////////////////////////////////////////
-		
-		//Wrapper for the invisible circles on top
-		var blobCircleWrapper = g.selectAll(".radarCircleWrapper")
-			.data(data.chart.groups)
-			.enter().append("g")
-			.attr("class", "radarCircleWrapper");
-			
-		//Append a set of invisible circles on top for the mouseover pop-up
-		blobCircleWrapper.selectAll(".radarInvisibleCircle")
-			.data(function(d,i) { return d; })
-			.enter().append("circle")
-			.attr("class", "radarInvisibleCircle")
-			.attr("r", cfg.dotRadius*1.5)
-			.attr("cx", function(d,i){ return rScale(d.value) * Math.cos(angleSlice*i - Math.PI/2); })
-			.attr("cy", function(d,i){ return rScale(d.value) * Math.sin(angleSlice*i - Math.PI/2); })
-			.style("fill", "none")
-			.style("pointer-events", "all")
-			.on("mouseover", function(d,i) {
-				var newX =  parseFloat(d3.select(this).attr('cx')) - 10;
-				var newY =  parseFloat(d3.select(this).attr('cy')) - 10;
-						
-				tooltip
-					.attr('x', newX)
-					.attr('y', newY)
-					.text(Format(d.value))
-					.transition().duration(200)
-					.style('opacity', 1);
-			})
-			.on("mouseout", function(){
-				tooltip.transition().duration(200)
-					.style("opacity", 0);
-			});
-			
-		//Set up the small tooltip for when you hover over a circle
-		var tooltip = g.append("text")
-			.attr("class", "tooltip")
-			.style("opacity", 0);
-		
-		/////////////////////////////////////////////////////////
-		/////////////////// Helper Function /////////////////////
-		/////////////////////////////////////////////////////////
-
-		//Taken from http://bl.ocks.org/mbostock/7555321
-		//Wraps SVG text	
-		function wrap(text, width) {
-		text.each(function() {
-			var text = d3.select(this),
-				words = text.text().split(/\s+/).reverse(),
-				word,
-				line = [],
-				lineNumber = 0,
-				lineHeight = 1.4, // ems
-				y = text.attr("y"),
-				x = text.attr("x"),
-				dy = parseFloat(text.attr("dy")),
-				tspan = text.text(null).append("tspan").attr("x", x).attr("y", y).attr("dy", dy + "em");
-				
-			while (word = words.pop()) {
-			line.push(word);
-			tspan.text(line.join(" "));
-			if (tspan.node().getComputedTextLength() > width) {
-				line.pop();
-				tspan.text(line.join(" "));
-				line = [word];
-				tspan = text.append("tspan").attr("x", x).attr("y", y).attr("dy", ++lineNumber * lineHeight + dy + "em").text(word);
-			}
-			}
-		});
-		}//wrap	
-
-	},[data]);
-
-	return <div ref={containerRef} style={ {width: "100%", height: "100%"} } />;
-}//RadarChart
+		{/* Stat Polygons */}
+		{data.chart.groups.map((group, i) => (
+			<g key={i} width={"100%"} height={"100%"} >
+				<path
+					d={attributeLine(group)}
+					fill={color(i)}
+					fillOpacity={0.35}
+					stroke={color(i)}
+					strokeWidth={2}
+				/>
+				{/* Points on the axes */}
+				{group.map((d, j) => (
+					<circle
+					key={j}
+					r={4}
+					cx={attributeScale(d.value) * Math.cos(sectionAngle * j - Math.PI / 2)}
+					cy={attributeScale(d.value) * Math.sin(sectionAngle * j - Math.PI / 2)}
+					fill={color(i)}
+					fillOpacity={0.8}
+					/>
+				))}
+			</g>
+		))}
+		</g>
+	</svg>
+	);
+}
 
 export default RadarChart;
