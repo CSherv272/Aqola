49f06ef3a1f15249aa006ff2076bc22642a9e13a
Michael Pettett <mjp@psyphi.net>
2026-04-27 14:13:47 +0100

    correct chart response name

--- frontend/aqola-frontend/app/lib/ChartModels.ts
+++ frontend/aqola-frontend/app/lib/ChartModels.ts
@@ -71,6 +71,6 @@ export type SpiderDiagramResponse = {
 
 // export type SpiderDiagramResponse = { axis: string; value: number; }[][];
 
-export type ChartData = BarChartResponse | LineChartResponse | SpiderChartResponse | PieChartResponse | null;
+export type ChartData = BarChartResponse | LineChartResponse | SpiderDiagramResponse | PieChartResponse | null;
 // type for determining which chart button is clicked
 export type ChartType = "line_over_time" | "bar_frequency" | "api_bar" | "line_over_time_by_lsoa";
