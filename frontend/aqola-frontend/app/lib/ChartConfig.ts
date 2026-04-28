63fda4cbc98898871d51ad101bfcdc505207968a
Callum Shervill <shervc272@gmail.com>
2026-04-28 01:56:13 +0100

    feat: updated polygon adaptibility to allow for zoom controlled swapping of area layers

--- frontend/aqola-frontend/app/lib/ChartConfig.ts
+++ frontend/aqola-frontend/app/lib/ChartConfig.ts
@@ -12,6 +12,42 @@ import { get_school_ofsted_history } from "./LineChart";
 
 type DatasetKey = keyof typeof datasetConfig;
 
+// type describes exactly what needs to be shown at what zoom level.
+type AreaLayer = {
+  areaType: "lsoa" | "postcode" | "urn";
+  minZoom: number;
+  maxZoom?: number;
+};
+
+type DatasetConfig = {
+  graphs: string[];
+  areaLayers: AreaLayer[];
+};
+
+const DATASET_CONFIG = datasetConfig as Record<string, DatasetConfig>;
+
+// Gets a Area Layer based on zoom and dataset.
+const resolveAreaType = (
+  datasetKey: string,
+  currentZoom: number,
+): AreaLayer | null => {
+  const config = DATASET_CONFIG[datasetKey];
+  if (!config) return null;
+
+  // Start from length to start with the most specific map features first.
+  for (let i = config.areaLayers.length - 1; i >= 0; i--) {
+    const layer = config.areaLayers[i];
+
+    //Infinity here so that if max zoom isn't given this still works.
+    const max = layer.maxZoom ?? Infinity;
+
+    if (currentZoom >= layer.minZoom && currentZoom < max) {
+      return layer;
+    }
+  }
+  return null; // This means they've zoomed out too far, or not set zoom properly
+};
+
 // Maps the apiCall part of the chart info to the actual API function
 // Every chart will need a new line here to get the data needed to populate the chart.
 
@@ -82,17 +118,11 @@ const getChartDefinition = (chartId: string) => {
   return chart;
 };
 
-const getDatasetAreaType = (dataset: string) => {
-  dataset = dataset.toLowerCase();
-  return (datasetConfig as Record<DatasetKey, Record<"areaType", string>>)[
-    dataset as DatasetKey
-  ]?.areaType;
-};
-
 export {
   getAvailableCharts,
   fetchChartData,
   getChartDefinition,
-  getDatasetAreaType,
+  resolveAreaType,
   type DatasetKey,
+  type AreaLayer,
 };
